c2199e016724ba9701365f0b0b354e5c92d41089
AnSantos99 <an.santos95@hotmail.com>
2019-11-20 15:53:39 +0000

    Exercicio1

---
+++ ExtendString/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace ExtendString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "ahahahhahahahah";
+            Console.WriteLine(a.ToRandomCase());
+        }
+    }
+
+
+    /// <summary>
+    /// Extension class of String
+    /// </summary>
+    public  static class StringExtension 
+    {
+
+        /// <summary>
+        /// Method extension to class string
+        /// </summary>
+        /// <param name="s"> To "say" its of class</param>
+        /// <returns> returns stringbuilder s</returns>
+        public static string ToRandomCase(this String s) 
+        {
+            // instance of type stringbuilder
+            StringBuilder sb = new StringBuilder();
+
+            // instance of type Random
+            Random rnd = new Random();
+
+            // Percorre todos os chars de uma string
+            foreach (char i in s) 
+            {
+                string cUpOrLow = i.ToString();
+
+                if (rnd.NextDouble() > 0.5)
+                {
+                    sb.Append(cUpOrLow.ToLower());
+                }
+
+                else 
+                {
+                    sb.Append(cUpOrLow.ToUpper());
+                }   
+            }
+            return sb.ToString();
+        }
+    }
+    
+}
